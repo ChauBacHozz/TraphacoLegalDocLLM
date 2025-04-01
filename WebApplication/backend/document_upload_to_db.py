ef725259c6d2119ac0d31b561ef7aa96d03101f8
ChauBacHozz <phongndsaiyan@gmail.com>
2025-04-01 02:17:26 +0000

    update

--- WebApplication/backend/document_upload_to_db.py
+++ WebApplication/backend/document_upload_to_db.py
@@ -2,13 +2,58 @@ from backend.graph_database.preprocessing.preprocess_docx import (extract_text, 
                                      convert_text_list_to_tree, flatten_tree,
                                      preprocess_chunks, normalize_appendix_text_bullets,
                                      normalize_modified_text_bullets)
-
 from backend.graph_database.db_uploader.save_doc_to_db import save_origin_doc_to_db, save_modified_doc_to_db
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 from icecream import ic
 from stqdm import stqdm
 from tqdm import tqdm
 import re
+
+def save_type1_origin_appendix_to_db(extracted_text, heading, doc_number, driver):
+    pass
+def save_type1_origin_pre_appendix_to_db(extracted_text, heading, doc_number, driver):
+    """
+        Hàm thực hiện tiền xử lý và sau đó lưu nội dung tiền mục lục của văn bản thông tư nghị định gốc 
+        vào cơ sở dữ liệu.
+
+        Args:
+            extracted_text: Đoạn văn bản thô đã được xử lý ở đoạn 
+    """
+    
+    heading_idx = None
+    # Kiểm tra xem trong 100 dòng đầu có xuất hiện từ "chương" không, nếu xuất hiện thì sẽ cắt nội dung
+    # từ khi xuất hiện từ "chương" cho đến cuối
+    for i, text in enumerate(extracted_text[:100]):
+        if "chương" in text.lower():
+            heading_idx = i
+            break
+
+    if heading_idx:
+        extracted_text = extracted_text[heading_idx:]
+    else:
+        print("ERROR")
+        return
+
+    full_text = normalize_bullets(extracted_text)
+    # Convert text list to tree base to manage content 
+    tree = convert_text_list_to_tree(full_text)
+    # Flatten tree into list of strings
+    flattened_tree = flatten_tree(tree)
+    # Split data into chunks
+    chunks = [text[0] for text in flattened_tree]
+    # chunks = [f"{path}: {text}" for path, text in flattened_tree]
+    # Preprocess chunks
+    preprocessed_chunks = preprocess_chunks(chunks, heading, doc_number)
+    # Extract 'text' atribute from preprocessed_chunks
+    texts = [chunk['content'] for chunk in preprocessed_chunks]
+    metadata_lst = []
+    for chunk in preprocessed_chunks:
+        # chunk.pop("content")
+        metadata_lst.append(chunk)
+    batch_size = 10    
+    for i in stqdm(range(0, len(metadata_lst), batch_size)):
+        save_origin_doc_to_db(texts[i:i+batch_size],metadata_lst[i:i+batch_size], driver)
+
 
 def save_modified_doc_pre_appendix_type1_to_db(extracted_text, heading, doc_number, driver):
     heading_idx = None
@@ -87,45 +132,6 @@ def save_modified_doc_pre_appendix_type2_to_db(extracted_text, heading, doc_numb
 
     for i in stqdm(range(0, len(metadata_lst), batch_size)):
         save_modified_doc_to_db(texts[i:i+batch_size],metadata_lst[i:i+batch_size], driver, doc_type=2)
-
-def save_origin_doc_pre_appendix_type1_to_db(extracted_text, heading, doc_number, driver):
-    heading_idx = None
-    for i, text in enumerate(extracted_text[:100]):
-        if "chương" in text.lower():
-            heading_idx = i
-            break
-
-    if heading_idx:
-        extracted_text = extracted_text[heading_idx:]
-    else:
-        print("ERROR")
-        return
-
-    full_text = normalize_bullets(extracted_text)
-    # Convert text list to tree base to manage content 
-    tree = convert_text_list_to_tree(full_text)
-    
-    # save_tree_to_db(tree, driver)
-    # Flatten tree into list of strings
-    flattened_tree = flatten_tree(tree)
-    # ic(flattened_tree)
-    # Split data into chunks
-    chunks = [text[0] for text in flattened_tree]
-    # chunks = [f"{path}: {text}" for path, text in flattened_tree]
-    # Preprocess chunks
-    preprocessed_chunks = preprocess_chunks(chunks, heading, doc_number)
-    # Extract 'text' atribute from preprocessed_chunks
-    texts = [chunk['content'] for chunk in preprocessed_chunks]
-    metadata_lst = []
-    for chunk in preprocessed_chunks:
-        # chunk.pop("content")
-        metadata_lst.append(chunk)
-
-    batch_size = 10    
-    print("☑️ saving pre-appendix data")
-
-    for i in stqdm(range(0, len(metadata_lst), batch_size)):
-        save_origin_doc_to_db(texts[i:i+batch_size],metadata_lst[i:i+batch_size], driver)
 
 def save_origin_doc_pre_appendix_type2_to_db(extracted_text, heading, doc_number, driver):
     heading_idx = None
@@ -230,7 +236,6 @@ def save_modified_doc_appendix_type1_to_db(document, heading, doc_number, driver
     ic(full_text)
     normalized_appendix_ids = [ids - appendix_ids[0] for ids in appendix_ids]
     chunks = normalize_appendix_text_bullets(full_text, normalized_appendix_ids)
-    ic(chunks)
     # # Convert text list to tree base to manage content 
     # tree = convert_text_list_to_tree(full_text)
     
--- WebApplication/pages/1_Upload_document.py
+++ WebApplication/pages/1_Upload_document.py
@@ -8,7 +8,10 @@ import os
 import sys
 from backend.graph_database.preprocessing.preprocess_docx import extract_text
 from neo4j import GraphDatabase
-from backend.document_upload_to_db import (save_modified_doc_appendix_type1_to_db,
+from backend.document_upload_to_db import (
+                                            save_type1_origin_appendix_to_db,
+                                            save_type1_origin_pre_appendix_to_db,
+                                            save_modified_doc_appendix_type1_to_db,
                                             save_modified_doc_pre_appendix_type1_to_db,
                                             save_origin_doc_pre_appendix_type1_to_db,
                                             save_origin_doc_appendix_type1_to_db,
@@ -43,14 +46,26 @@ upload_files = st.file_uploader(
     Văn bản pháp luật gồm hai loại:
         - Văn bản thông tư nghị định: Được định nghĩa là loại 1 (type 1)
         - Văn bản luật (e.g Luật Dược): Được định nghĩa là loại 2 (type 2)
+
     Cấu trúc mỗi văn bản dù là loại 1 hay loại 2 đều gồm cấu trúc hai phần:
         - Nội dung chính - nằm ở phía trước mục lục (main_content)
-        - Mục lục (appendix)
+        - Phụ lục (appendix)
+
     Mỗi văn bản sẽ được chia thành hai loại văn bản:
         - Văn bản gốc (origin_doc)
         - Văn bản bãi bỏ/chỉnh sửa/bổ sung (modified_doc)
-    Như vậy có 4 trường hợp xảy ra:
-        - 
+
+    Như vậy có 8 trường hợp xảy ra:
+        - save_type1_origin_appendix_to_db: Lưu nội dung phụ lục của văn bản thông tư nghị định gốc
+        - save_type1_origin_pre_appendix_to_db: Lưu nội dung tiền phụ lục của văn bản thông tư nghị định gốc
+        - save_type1_modified_appendix_to_db: Lưu nội dung phụ lục của văn bản thông tư nghị định chỉnh sửa
+        - save_type1_modified_pre_appendix_to_db: Lưu nội dung tiền phụ lục của văn bản thông tư nghị định chỉnh sửa
+
+        - save_type2_origin_appendix_to_db: Lưu nội dung phụ lục của văn bản luật gốc
+        - save_type2_origin_pre_appendix_to_db: Lưu nội dung tiền phụ lục của văn bản luật gốc
+        - save_type2_modified_appendix_to_db: Lưu nội dung phụ lục của văn bản luật chỉnh sửa
+        - save_type2_modified_pre_appendix_to_db: Lưu nội dung tiền phụ lục của văn bản luật chỉnh sửa
+
     Workflow khi upload document như sau:
         - Bóc tách từ file sang text, lấy ra thông tin về phụ lục (là index của dòng 
         bắt đầu phụ lục, nếu không có phụ lục thì biến đó sẽ là None)
@@ -60,46 +75,49 @@ upload_files = st.file_uploader(
 if st.button("Upload to database"):
     for upload_file in upload_files:
         doc_file = docx.Document(upload_file)
-        # Extract text from doc
+        # Trích xuất dữ liệu văn bản từ doc_file, khi trích xuất sẽ rà soát xem có phụ lục tồn tại không, nếu có thì chỉ mục (theo dòng)
+        # của phục lục sẽ được lưu vào biến appendiz_index, nếu không appendix_index sẽ nhận về None
         extracted_text, appendix_index = extract_text(doc_file)
-        # Remove special text from extracted_text
+
         if appendix_index != None:
+            # Chia văn bản đầu thành văn bản tiền phụ lục và văn bản phụ lục
             pre_appendix_text = extracted_text[:appendix_index - 1]
             appendix_text = extracted_text[appendix_index - 1:]
         else:
+            # Trường hợp còn lại toàn bộ văn bản được coi là văn bản tiền phụ lục
             pre_appendix_text = extracted_text
-        # Extract document heading
+        # Trích xuất thông tin tiêu đề văn bản và mã văn bản
         doc_number = doc_file.tables[0].rows[1].cells[0].text
         heading = ": ".join(extracted_text[:2])
-        # Type 1
+
+        # Kiểm tra nếu văn bản có là thông tư nghị định hay không (loại 1)
         if "nghị định" in heading.lower() or "thông tư" in heading.lower():
-            # if "sửa đổi" in heading.lower():
-            if "sửa đổi" in heading.lower() or "bổ sung" in heading.lower() or "bãi bỏ" in heading.lower():
-                # Văn bản sửa đổi
-                if appendix_index != None:
-                    print("Có phụ lục")
-                    save_modified_doc_appendix_type1_to_db(doc_file, heading, doc_number, driver)
-                save_modified_doc_pre_appendix_type1_to_db(pre_appendix_text, heading, doc_number, driver)
-            else:
+            # Kiểm tra có phải văn bản sửa đổi hây văn bản gốc
+            if "sửa đổi" not in heading.lower() and "bổ sung" not in heading.lower() and "bãi bỏ" not in heading.lower():
                 # Văn bản gốc
-                if appendix_index != None:
-                    print("Có phụ lục")
-                    save_origin_doc_appendix_type1_to_db(doc_file, heading, doc_number, driver)
-                save_origin_doc_pre_appendix_type1_to_db(pre_appendix_text, heading, doc_number, driver)
-                st.toast(f"Saved {upload_file.name} database✅")
-        # Type 2
+                if appendix_index:
+                    save_type1_origin_appendix_to_db(appendix_index, heading, doc_number, driver)
+                save_type1_origin_pre_appendix_to_db(pre_appendix_text, heading, doc_number, driver)
+            else:
+                # Văn bản sửa đổi
+                if appendix_index:
+                    save_type1_modified_appendix_to_db(appendix_index, heading, doc_number, driver)
+                save_type1_modified_pre_appendix_to_db(pre_appendix_text, heading, doc_number, driver)
+
+        # Kiểm tra nếu văn bản có là luật hay không (loại 2)
         elif "luật" in heading.lower():
-            if "sửa đổi" in heading.lower() or "bổ sung" in heading.lower() or "bãi bỏ" in heading.lower():
-                # Văn bản sửa đổi
-                save_modified_doc_pre_appendix_type2_to_db(pre_appendix_text, heading, doc_number, driver)
-            else:
+            if "sửa đổi" not in heading.lower() and "bổ sung" not in heading.lower() and "bãi bỏ" not in heading.lower():
                 # Văn bản gốc
-                if appendix_index != None:
-                    print("Có phụ lục")
-                    save_origin_doc_appendix_type2_to_db(doc_file, heading, doc_number, driver)
-                save_origin_doc_pre_appendix_type2_to_db(pre_appendix_text, heading, doc_number, driver)
-                st.toast(f"Saved {upload_file.name} database✅")
+                if appendix_index:
+                    save_type2_origin_appendix_to_db(appendix_index, heading, doc_number, driver)
+                save_type2_origin_pre_appendix_to_db(pre_appendix_text, heading, doc_number, driver)
+            else:
+                # Văn bản sửa đổi
+                if appendix_index:
+                    save_type2_modified_appendix_to_db(appendix_index, heading, doc_number, driver)
+                save_type2_modified_pre_appendix_to_db(pre_appendix_text, heading, doc_number, driver)
+            st.toast(f"Saved {upload_file.name} database✅")
+        # Nếu không phải thông tư nghị định và cũng không phải luật, in ra lỗi
         else:
-            print("ERROR")
-        # Merge bullet from extracted text
+            raise ValueError("Loại văn bản không được hỗ trọ")
 
